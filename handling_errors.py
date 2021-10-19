e042b8b0331a1e870483d5108098970265148cae
ghostmonk <nicholas@ghostmonk.com>
2021-10-19 15:08:42 -0400

    Added better error handling

--- handling_errors.py
+++ handling_errors.py
@@ -4,11 +4,15 @@ file_name = 'misc/errors.txt'
 
 try:
     with open(file_name, 'x') as a_file:
-        a_file.write(b'Meatballs\n')
+        a_file.write('Meatballs\n')
 except FileExistsError as err:
     print(f"The file {file_name} already exists!!")
     sys.exit(1)
 except:
     print(f"Unable to write to file")
     sys.exit(1)
+else:
+    print(f"Write to {file_name}")
+finally:
+    print("Execution complete")
 
